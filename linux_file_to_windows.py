49f8846dc0d2222873a15457a56b06be531970d1
ygm <ygm.github@gmail.com>
2017-05-03 02:39:36 +0900

    convert linux's new line character to windows

---
+++ linux_file_to_windows.py
@@ -0,0 +1,10 @@
+fileList = ['fname']
+
+for fname in fileList:    
+    with open(fname,'r') as f:
+        con = f.read()
+    
+    con = con.replace('/n','\r\n')
+    
+    with open(fname,'w') as f:
+        f.write(con)
