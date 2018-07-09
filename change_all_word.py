e26f6d3a800f406890ae57994ab4b05010a146ac
양광민 <yang-gwangmin@yang-gwangmin-ui-MacBook-Pro.local>
2018-07-09 21:04:40 +0900

    update

--- change_all_word.py
+++ change_all_word.py
@@ -1,14 +1,28 @@
 import os
+import sys
 
-start_path = '.'
-word = {'before':'after'}
+# Change before to after in file named fname.
+def change_word(fname,before,after):
+	con = None
 
-for path,dirs,files in os.walk(start_path):
-    for fname in files:
-        con = None
-        with open(fname,'r') as f:
-            con = f.read()
-        for before in word.keys():
-            con = con.replace(before,word[before])
-        with open(fname,'w') as f:
-            f.write(con)
+	with open(fname,'r') as f:
+		con = f.read()
+	
+	con = con.replace(before,after)
+
+	with open(fname,'w') as f:
+		f.write(con)
+
+# Print this program's help message
+def help_msg():
+	print 'python '+sys.argv[0]+' [before] [after]'
+
+if sys.argv[1] == '-h':
+	help_msg()
+	exit(0)
+
+for path,dirs,files in os.walk('.'):
+	for fname in files:
+		if fname != sys.argv[0]:
+			change_word(fname,sys.argv[1],sys.argv[2])
+
