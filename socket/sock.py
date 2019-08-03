eca2d847040f4e3fbdecf0d597c92d0949b11f6d
gwangmin <ygm.gwangmin@gmail.com>
2019-08-04 02:29:07 +0900

    socket

--- socket/sock.py
+++ socket/sock.py
@@ -24,110 +24,121 @@ from socket import *
 
 class sock(object):
     '''
-    Create TCP or UDP socket and print log.
+    Socket wrapper
     '''
-	def __init__(self,type_='tcp'):
+    def __init__(self,type_='tcp'):
         '''
         Initializer
         
-        type: (Optional) socket type. 'tcp' or 'udp'. Default 'tcp'.
+        type: (Optional) Socket type or python socket. 'tcp' or 'udp' or python socket. If python socket, copy and set tcp type. Default 'tcp'.
         '''
-		self.type = type_
-		if type_ == 'tcp':
-			self.sock = socket(AF_INET,SOCK_STREAM)
-			print('[*] Create TCP socket')
-		elif type_ == 'udp':
-			self.sock = socket(AF_INET,SOCK_DGRAM)
-			print('[*] Create UDP socket')
+        self.type = type_
+        if type_ == 'tcp':
+            self.sock = socket(AF_INET,SOCK_STREAM)
+            print('[*] Create TCP socket')
+        elif type_ == 'udp':
+            self.sock = socket(AF_INET,SOCK_DGRAM)
+            print('[*] Create UDP socket')
+        else:
+            self.type = 'tcp'
+            self.sock = type_
             
-	def bind(self,ip,port):
+    def bind(self,ip,port):
         '''
         Binding socket to specified address and print log.
         
         ip: Local ip. string.
         port: Local port. int.
         '''
-		self.sock.bind((ip, port))
-		print('[*] Binding '+ip+':'+str(port))
+        self.sock.bind((ip, port))
+        print('[*] Binding '+ip+':'+str(port))
         
-	def listen(self,backlog=3):
+    def listen(self,backlog=3):
         '''
         Listen and print log.
         
         backlog: (Optional) backlog queue size. Default 3.
         '''
-		self.sock.listen(backlog)
-		print('[*] Listen with backlog queue '+str(backlog))
+        self.sock.listen(backlog)
+        print('[*] Listen with backlog queue '+str(backlog))
         
-	def accept(self):
+    def accept(self):
         '''
         Accept and print log.
         '''
-		client_sock, addr = self.sock.accept()
-		print('[*] Accepted',str(addr))
-		return client_sock, addr
+        client_sock, addr = self.sock.accept()
+        print('[*] Accepted '+str(addr))
+        client_sock = sock(client_sock)
+        return client_sock, addr
     
-	def connect(self,ip,port):
+    def connect(self,ip,port):
         '''
         Connect to specified address and print log.
         
         ip: Remote ip. string.
         port: Remote port. int.
         '''
-		addr = (ip, port)
-		try:
-			self.sock.connect(addr)
-			print('[*] Connected '+ip+':'+str(port))
-		except:
-			print('[!] Connection to '+ip+':'+str(port)+' failed')
-        
-	def send(self,msg,ip=None,port=None):
+        addr = (ip, port)
+        try:
+            self.sock.connect(addr)
+            print('[*] Connected '+ip+':'+str(port))
+        except Exception as e:
+            print('[!] Connection to '+ip+':'+str(port)+' failed')
+            print(e)
+            
+    def send(self,msg,ip=None,port=None):
         '''
         Send message and print log.
         If UDP, require addr.
         
+        msg: Message. string
         ip: (udp) Remote ip. string
         port: (udp) Remote port. int
         '''
-		if self.type == 'tcp':
-			try:
-				self.sock.send(msg)
-				print('[*] Send: '+msg)
-			except:
-				print('[!] send() error')
-		elif self.type == 'udp':
-			addr = (ip,port)
-			try:
-				self.sock.sendto(msg,addr)
-				print('[*] Send: '+msg+' to '+str(addr))
-			except:
-				print('[!] send() error')
+        if self.type == 'tcp':
+            try:
+                self.sock.send(msg.encode('utf-8'))
+                print('[*] Send: '+msg)
+            except Exception as e:
+                print('[!] send() error')
+                print(e)
+        elif self.type == 'udp':
+            addr = (ip,port)
+            try:
+                self.sock.sendto(msg.encode('utf-8'),addr)
+                print('[*] Send: '+msg+' to '+str(addr))
+            except Exception as e:
+                print('[!] send() error')
+                print(e)
                 
-	def recv(self,buf_size=1024):
+    def recv(self,buf_size=1024):
         '''
         Receive data and return. print log.
         
         buf_size: (Optional) Buffer size. Default 1024.
         '''
-		if self.type == 'tcp':
-			try:
-				data = self.sock.recv(buf_size)
-				print('[*] Receive:',data)
-				return data
-			except:
-				print('[!] recv() error')
-		elif self.type == 'udp':
-			try:
-				data = self.sock.recvfrom(buf_size)
-				print('[*] Receive:',data)
-				return data
-			except:
-				print('[!] recv() error')
+        if self.type == 'tcp':
+            try:
+                data = self.sock.recv(buf_size).decode('utf-8')
+                print('[*] Receive: '+data)
+                return data
+            except Exception as e:
+                print('[!] recv() error')
+                print(e)
+        elif self.type == 'udp':
+            try:
+                data = self.sock.recvfrom(buf_size).decode('utf-8')
+                print('[*] Receive: '+data)
+                return data
+            except Exception as e:
+                print('[!] recv() error')
+                print(e)
                 
-	def close(self):
+    def close(self):
         '''
         Close socket and print log.
         '''
-		self.sock.close()
-		print('[*] Close socket')
-        
+        self.sock.close()
+        print('[*] Close socket')
+
+
