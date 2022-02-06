4122a29140ee7511ebe828a44df055059132425b
ygm <ygm.gwangmin@gmail.com>
2022-02-06 19:23:55 +0900

    Update time.js

--- time/time.js
+++ time/time.js
@@ -1,10 +1,18 @@
 /**
- * Sleep
+ * Sleep(sync)
  * @param {*} ms delay
  */
 function sleep(ms) {
     const wakeUpTime = Date.now() + ms;
     while (Date.now() < wakeUpTime) {}
+}
+
+/**
+ * Delay
+ * @param {*} ms delay
+ */
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 console.log('start');
